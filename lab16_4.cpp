90158d3472caf0a2a437a8f90fb9aa00e6d8bfde
PHANAGUS <phanarin03072002@gmail.com>
2022-01-17 02:36:41 +0700

    lab16_4.cpp completed

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,53 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d)
+{
+	const int m1 = a, m2 = b, m3 = c, m4 = d;
+	a=0;b=0;c=0;d=0;
+	int x = rand()%4 + 1;
+	switch(x)
+	{
+		case 1:a = m1; break;
+		case 2:a = m2; break;
+		case 3:a = m3; break;
+		case 4:a = m4; break;
+	}
+	for(;;)
+	{
+		int i = rand()%4 + 1;
+		switch(i)
+		{
+			case 1:b = m1; break;
+			case 2:b = m2; break;
+			case 3:b = m3; break;
+			case 4:b = m4; break;
+		}
+		if(b!=a) break;
+	}
+	for(;;)
+	{
+		int i = rand()%4 + 1;
+		switch(i)
+		{
+			case 1:c = m1; break;
+			case 2:c = m2; break;
+			case 3:c = m3; break;
+			case 4:c = m4; break;
+		}
+		if(c!=a && c!=b) break;
+	}
+	for(;;)
+	{
+		int i = rand()%4 + 1;
+		switch(i)
+		{
+			case 1:d = m1; break;
+			case 2:d = m2; break;
+			case 3:d = m3; break;
+			case 4:d = m4; break;
+		}
+		if(d!=a && d!=b && d!=c) break;
+	}
+}
